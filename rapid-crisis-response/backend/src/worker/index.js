c846cd0dda577634ca58ad9b3970562ceb1da20b
PRAVEEN KUMAR JAYSWAL <praveenjayswal625@gmail.com>
2026-04-02 23:23:09 +0530

    Removed duplicate folder

--- rapid-crisis-response/backend/src/worker/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { startScheduler } = require('./scheduler');
-
-const redisConfig = {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-};
-
-startScheduler(redisConfig);
